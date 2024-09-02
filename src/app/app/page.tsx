2c28fad5ecbcd9b1211f730bd3e7299180583f84
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-09-02 12:55:20 +0900

    feat: redirection to app if auth is completed

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,6 @@
-export default function App() {
-  return <h1>app</h1>;
+import { auth } from "@/services/auth";
+
+export default async function App() {
+  const session = await auth();
+  return <h1>{session?.user?.email}</h1>;
 }
